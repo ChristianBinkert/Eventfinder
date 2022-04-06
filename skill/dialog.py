3df3e7adfadd4fe82ae9267e22a2343b5b6e06aa
c.binkert.freiburg@gmail.com <c.binkert.freiburg@gmail.com>
2022-04-06 08:55:07 +0200

    docs: consistent event naming in dialog file

--- skill/dialog.py
+++ skill/dialog.py
@@ -1,37 +1,4 @@
-# Copyright 2021, Mycroft AI Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Abstraction of dialog building for the weather skill.
 
-There are A LOT of dialog files in this skill.  All the permutations of timeframe,
-weather condition and location add up fast.  To help with discoverability, a naming
-convention was applied to the dialog files:
-    <timeframe>-<weather info>-<qualifier>-<locale>.dialog
-
-    Example:
-         daily-temperature-high-local.dialog
-
-    * Timeframe: the date or time applicable to the forecast.  This skill supports
-        current, hourly and daily weather.
-    * Weather info: a description of what type of weather the dialog refers to.
-        Examples include "temperature", "weather" and "sunrise".
-    * Qualifier: further qualifies what type of weather is being reported.  For
-        example, temperature can be qualified by "high" or "low".
-    * Locale: indicates if the dialog is for local weather or weather in a remote
-        location.
-
-The skill class will use the "name" and "data" attributes to pass to the TTS process.
-"""
 from typing import List, Tuple
 
 from mycroft.util.format import join_list, nice_number, nice_time
@@ -41,20 +8,15 @@ from .intent import EventIntent
 from .util import get_speakable_day_of_week, get_time_period
 from .events import (
     CURRENT,
-    CurrentWeather,
+    CurrentEvents,
     DAILY,
     DailyWeather,
     HOURLY,
 )
 
-# TODO: MISSING DIALOGS
-#   - current.clear.alternative.local
-#   - current.clouds.alternative.local
-#   - daily.snow.alternative.local
-#   - all hourly.<condition>.alternative.local/location
-#   - all hourly.<condition>.not.expected.local/location
-class WeatherDialog:
-    """Abstract base class for the weather dialog builders."""
+
+class EventDialog:
+    """Abstract base class for the event dialog builders."""
 
     def __init__(self, intent_data: EventIntent, config: EventConfig):
         self.intent_data = intent_data
@@ -85,28 +47,28 @@ class WeatherDialog:
             self.data.update(location=spoken_location)
 
 
-class CurrentDialog(WeatherDialog):
-    """Weather dialog builder for current weather."""
+class CurrentDialog(EventDialog):
+    """Event dialog builder for current events."""
 
     def __init__(
-        self, intent_data: EventIntent, config: EventConfig, weather: CurrentWeather
+        self, intent_data: EventIntent, config: EventConfig, events: CurrentEvents
     ):
         super().__init__(intent_data, config)
-        self.weather = weather
+        self.events = events
         self.name = CURRENT
 
     def build_event_dialog(self):
-        """Build the components necessary to speak current weather."""
+        """Build the components necessary to speak current events."""
         self.name += "-event"
         self.data = dict(
-            event_name=self.weather.event_name,
-            localDate=self.weather.localDate,
+            event_name=self.events.event_name,
+            localDate=self.events.localDate,
             temperature_unit=self.config.temperature_unit,
         )
         self._add_location()
 
 
-class DailyDialog(WeatherDialog):
+class DailyDialog(EventDialog):
     """Weather dialog builder for daily weather."""
 
     def __init__(
@@ -127,52 +89,6 @@ class DailyDialog(WeatherDialog):
         )
         self._add_location()
 
-    def build_temperature_dialog(self, temperature_type: str):
-        """Build the components necessary to speak the daily temperature.
-
-        :param temperature_type: indicates if temperature is day, high or low
-        """
-        self.name += "-temperature"
-        if temperature_type == "high":
-            self.name += "-high"
-            self.data = dict(temperature=self.weather.temperature.high)
-        elif temperature_type == "low":
-            self.name += "-low"
-            self.data = dict(temperature=self.weather.temperature.low)
-        else:
-            self.data = dict(temperature=self.weather.temperature.day)
-        self.data.update(
-            day=get_speakable_day_of_week(self.weather.date_time),
-            temperature_unit=self.intent_data.unit or self.config.temperature_unit,
-        )
-        self._add_location()
-
-
-
-class WeeklyDialog(WeatherDialog):
-    """Weather dialog builder for weekly weather."""
-
-    def __init__(
-        self,
-        intent_data: EventIntent,
-        config: EventConfig,
-        forecast: List[DailyWeather],
-    ):
-        super().__init__(intent_data, config)
-        self.forecast = forecast
-        self.name = "weekly"
-
-    def build_temperature_dialog(self):
-        """Build the components necessary to temperature ranges for a week."""
-        low_temperatures = [daily.temperature.low for daily in self.forecast]
-        high_temperatures = [daily.temperature.high for daily in self.forecast]
-        self.name += "-temperature"
-        self.data = dict(
-            low_min=min(low_temperatures),
-            low_max=max(low_temperatures),
-            high_min=min(high_temperatures),
-            high_max=max(high_temperatures),
-        )
 
 
 def get_dialog_for_timeframe(timeframe: str, dialog_ags: Tuple):
--- skill/events.py
+++ skill/events.py
@@ -1,16 +1,4 @@
-# Copyright 2021, Mycroft AI Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 """Representations and conversions of the data returned by the weather API."""
 from datetime import timedelta
 import time
@@ -29,33 +17,16 @@ HOURLY = "hourly"
 SATURDAY = 5
 SUNDAY = 6
 
-# Condition Icons (see https://openweathermap.org/weather-conditions)
-#   Map each of the possible weather condition icon codes from OpenWeatherMap to an
-#   image/animation file used by the GUI.  The icon codes contain a number and a letter.
-#   A "n" after the number indicates night and a "d" indicates day.
-#
-#   The icon/image map is used by the Mark II, which does not use animations for
-#   performance reasons.  The icon/animation map is used by the scalable QML.  The
-#   icon/code map is for the Mark I, which accepts a code to determine what
-#   is displayed.
 
 
-
-class CurrentWeather:
+class CurrentEvents:
     """Abstract data representation of commonalities in forecast types."""
     #--------------------------------------------------------------------------------------------- here
     def __init__(self, eventname: str, localdate:str, timezone: str):
         self.date_time = convert_to_local_datetime(int(time.time()), timezone)
         self.event_name = eventname
         self.localDate = localdate
-        """self.feels_like = weather["feelsLike"]
-        self.pressure = weather["pressure"]
-        self.humidity = weather["humidity"]
-        self.dew_point = weather["dewPoint"]
-        self.clouds = weather["clouds"]
-        self.wind_speed = int(weather["windSpeed"])
-        self.wind_direction = self._determine_wind_direction(weather["windDeg"])
-        self.condition = WeatherCondition(weather["weather"][0])"""
+
 
 
 class DailyWeather:
@@ -66,11 +37,6 @@ class DailyWeather:
         self.event_name = report['name']
         self.localDate = report['dates']['start']['localDate']
 
-        """self.sunrise = convert_to_local_datetime(weather["sunrise"], timezone)
-        self.sunset = convert_to_local_datetime(weather["sunset"], timezone)
-        self.temperature = will thDailyTemperature(weather["temp"])
-        self.feels_like = DailyFeelsLike(weather["feelsLike"])
-        self.chance_of_precipitation = int(weather["pop"] * 100)"""
 
 
 class EventReport:
@@ -82,7 +48,7 @@ class EventReport:
         timezone = report[0]["dates"].get("timezone")
         eventname = report[0].get("name")
         localdate = report[0]['dates'].get('start').get('localDate')
-        self.current = CurrentWeather(eventname, localdate, timezone)
+        self.current = CurrentEvents(eventname, localdate, timezone)
         #--------------------------------------------------------------------------------------------- here
         self.daily = [DailyWeather(event, timezone) for event in report[:10]]
         today = self.daily[0]
@@ -101,72 +67,3 @@ class EventReport:
             weather = self.current
 
         return weather
-
-
-    def get_forecast_for_multiple_days(self, days: int) -> List[DailyWeather]:
-        """Use the intent to determine which daily forecast(s) satisfies the request.
-
-        Args:
-            days: number of forecast days to return
-
-        Returns:
-            list of daily forecasts for the specified number of days
-
-        Raises:
-            IndexError when number of days is more than what is returned by the API
-        """
-        if days > 7:
-            raise IndexError("Only seven days of forecasted weather available.")
-
-        forecast = self.daily[1 : days + 1]
-
-        return forecast
-
-
-    def get_weekend_forecast(self):
-        """Use the intent to determine which daily forecast(s) satisfies the request.
-
-        Returns:
-            The Saturday and Sunday forecast from the list of daily forecasts
-        """
-        forecast = list()
-        for forecast_day in self.daily:
-            report_date = forecast_day.date_time.date()
-            if report_date.weekday() in (SATURDAY, SUNDAY):
-                forecast.append(forecast_day)
-
-        return forecast
-
-    def get_next_precipitation(self, intent_data):
-        """Determine when the next chance of precipitation is in the forecast.
-
-        Args:
-            intent_data: Parsed intent data
-
-        Returns:
-            The weather report containing the next chance of rain and the timeframe of
-            the selected report.
-        """
-        report = None
-        current_precipitation = True
-        timeframe = HOURLY
-        for hourly in self.hourly:
-            if hourly.date_time.date() > intent_data.location_datetime.date():
-                break
-            if hourly.chance_of_precipitation > THIRTY_PERCENT:
-                if not current_precipitation:
-                    report = hourly
-                    break
-            else:
-                current_precipitation = False
-
-        if report is None:
-            timeframe = DAILY
-            for daily in self.daily:
-                if daily.date_time.date() == intent_data.location_datetime.date():
-                    continue
-                if daily.chance_of_precipitation > THIRTY_PERCENT:
-                    report = daily
-                    break
-
-        return report, timeframe
